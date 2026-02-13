56c860841b00c10ab183e1687a0c95d2cb131fe8
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-02-13 12:15:27 +0200

    add script for running swiftlint

---
+++ scripts/run-swiftlint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+REPORT_FILE="$SCRIPT_DIR/swiftlint-results_${TIMESTAMP}.txt"
+
+cd "$REPO_ROOT"
+
+git ls-files --cached --others --exclude-standard --full-name -- '*.swift' | while read -r file; do
+    swiftlint lint "$file"
+done | tee "$REPORT_FILE"
