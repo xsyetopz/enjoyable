73f853deed24a382b6a4835956718a42f23e409f
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-02-13 16:40:22 +0200

    delete previous

--- scripts/run-swiftlint.sh
+++ scripts/run-swiftlint.sh
@@ -6,6 +6,7 @@ TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 REPORT_FILE="$SCRIPT_DIR/swiftlint-results_${TIMESTAMP}.txt"
 
 cd "$REPO_ROOT"
+rm -f "$SCRIPT_DIR"/swiftlint-results_*.txt
 
 git ls-files --cached --others --exclude-standard --full-name -- '*.swift' | while read -r file; do
     swiftlint lint "$file"
