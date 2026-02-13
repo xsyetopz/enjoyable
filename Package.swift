39a209e3084742ddb43a242140a3dad9d9f4a189
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-02-13 22:55:46 +0200

    move to main PM

--- Package.swift
+++ Package.swift
@@ -7,7 +7,28 @@ public let kPackage = Package(
   platforms: [
     .macOS(.v14)
   ],
-  products: [],
-  dependencies: [],
-  targets: []
+  products: [
+    .library(name: "SwiftUSB", targets: ["SwiftUSB"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.6.0")
+  ],
+  targets: [
+    .target(
+      name: "SwiftUSB",
+      dependencies: [
+        "CLibUSB",
+        .product(name: "Logging", package: "swift-log")
+      ],
+      path: "Modules/SwiftUSB",
+    ),
+    .systemLibrary(
+      name: "CLibUSB",
+      pkgConfig: "libusb-1.0",
+      providers: [
+        .brew(["libusb"]),
+        .apt(["libusb-1.0-0-dev"])
+      ]
+    )
+  ]
 )
